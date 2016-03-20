de74cd65bf231a4aa06264ec70042d3bd8a1c8fd
Lucas David <lucasolivdavid@gmail.com>
2016-03-20 10:13:53 -0300

    Add continuous integration config files.
      Add travis config files.
      Add coverage dependency.
      Add tests/requirements.txt

--- setup.py
+++ setup.py
@@ -8,15 +8,17 @@ config = {
     'name': 'artificial',
     'description': 'Implementation for Artificial Intelligence: '
                    'An Modern Approach book examples.',
+    'long_description': open('README.md').read(),
     'author': 'Lucas David',
-    'url': '',
-    'download_url': '',
     'author_email': 'lucasolivdavid@gmail.com',
+    'url': 'https://github.com/lucasdavid/artificial',
+    'download_url': 'https://github.com/lucasdavid/artificial/'
+                    'archive/master.zip',
     'version': '0.1',
-    'install_requires': ['nose'],
+    'install_requires': [],
+    'tests_require': ['nose', 'coverage'],
     'packages': ['artificial'],
     'scripts': [],
 }
 
 setup(**config)
-
