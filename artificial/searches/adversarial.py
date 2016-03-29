2fb63539bbd42a103788fcbca6b5b08ad8224142
Lucas David <lucasolivdavid@gmail.com>
2016-03-29 18:00:08 -0300

    Add Random search

--- artificial/searches/adversarial.py
+++ artificial/searches/adversarial.py
@@ -1,11 +1,12 @@
 import time
+import random
 import numpy as np
 
 from . import base
 
 
-class MinMax(base.Search):
-    """Min Max Adversarial Search.
+class Adversarial(base.Search, metaclass=abc.ABCMeta):
+    """Adversarial Search.
     
     Parameters
     ----------
@@ -26,6 +27,53 @@ class MinMax(base.Search):
         Time in which performance started.
         `time.time() - started_at` yeilds how much time has
         approximately passed since the `MinMax.perform` was called.
+       
+    """
+
+    MINIMIZE, MAXIMIZE = (0, 1)
+    
+    def __init__(self, agent, root=None,
+                 time_limit=np.inf, depth_limit=np.inf, 
+                 dispose=False):
+        super().__init__(agent=agent, root=root)
+
+        self.time_limit = time_limit
+        self.depth_limit = depth_limit
+        self.dispose = dispose
+        
+        self.started_at = None
+
+    def perform(self):
+        self.started_at = time.time()
+        return super().perform()
+
+
+class Random(Adversarial):
+    """Random Adversarial Search.
+    
+    Actions are taken randomly, achieving a result.
+    
+    """
+    def __init__(self, agent, root=None,
+                 time_limit=np.inf, depth_limit=np.inf,
+                 dispose=None, random_state=None)
+         self.random_state = random_state or random.Random()
+
+    def _perform(self):
+        state = self.root
+        depth = 0
+        
+        while state and depth < self.limit_depth and \
+              time.time() - self.started_at < self.time_limit:
+            children = self.agent.predict(state)
+            state = self.random_state.choice(children) if children else None
+            depth += 1
+
+        return state
+
+
+class MinMax(Adversarial):
+    """Min Max Adversarial Search.
     
     Notes
     -----
@@ -36,33 +84,21 @@ class MinMax(base.Search):
 
     """
     
-    MINIMIZE, MAXIMIZE = (0, 1)
-
-    def __init__(self, agent, root=None,
-                 time_limit=np.inf, depth_limit=np.inf,
-                 dispose=False):
-        super().__init__(agent=agent, root=root)
-        
-        self.time_limit = time_limit
-        self.dispose = dispose
-        self.started_at = None
-       
     def _perform(self):
-        self.started_at = time.time()
-        return self._min_max(self.root, 0, self.MAXIMIZE)
+        return self._min_max_policy(self.root, 0, self.MAXIMIZE)
     
-    def _min_max(self, state, depth):
+    def _min_max_policy(self, state, depth):
         if self.depth_limit and depth > self.depth_limit or \
            time.time() - self.started_at > self.time_limit:
             return self.agent.utility(self)
 
         children = self.agent.predict(state)
-        
+
         if not children:
             # Terminal state. Return utility.
             return self.agent.utility(state)
 
-        utilities = [self._min_max(c, depth + 1) for c in children]
+        utilities = [self._min_max_policy(c, depth + 1) for c in children]
         order = max if depth % 2 == self.MAXIMIZE else min
 
         return order(children, keys=lambda i, e: utilities[i])
---
+++ artificial/searches/tests/adversarial_test.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+from artificial.searches import adversarial
+
+
+class _UtilityTestAgent(agents.UtilityAgent):
+    def predict(self):
+        pass
+
+
+class RandomTest(TestCase):
+    def SetUp(self):
+        self.agent = 
+        
+    def test_sanitize(self):
+        
+        adversarial.Random()
+
